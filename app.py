eb3c1d3e2119e74e52f58f7eedd159075d04bc3a
Kislay Kumar <kislayreigns20092002@gmail.com>
2026-06-09 21:20:03 +0530

    initial commit of the day

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
 import streamlit as st
 import os
-from analyzer.transcriber import transcribe_audio
+import librosa
 
-# -------------------------------
-# Page Configuration
-# -------------------------------
+from analyzer.transcriber import transcribe_audio
+from analyzer.metrics import (
+    get_word_count,
+    calculate_wpm,
+    get_speed_category,
+    detect_fillers
+)
+
+# -----------------------------------
+# Page Config
+# -----------------------------------
+
 st.set_page_config(
     page_title="Smart Classroom Speech Analytics",
     layout="wide"
 )
 
-# -------------------------------
-# Create Upload Folder
-# -------------------------------
 os.makedirs("uploads", exist_ok=True)
 
-# -------------------------------
-# Title
-# -------------------------------
 st.title("🎙 Smart Classroom Speech Analytics")
 
-# -------------------------------
-# File Upload
-# -------------------------------
 uploaded_file = st.file_uploader(
     "Upload Audio",
     type=["wav", "mp3", "m4a"]
@@ -40,13 +40,55 @@ if uploaded_file:
 
     st.audio(file_path)
 
-    if st.button("Transcribe"):
+    if st.button("Transcribe & Analyze"):
 
-        with st.spinner("Transcribing Audio..."):
+        with st.spinner("Analyzing Audio..."):
 
             transcript = transcribe_audio(file_path)
 
-        st.success("Transcription Complete!")
+            duration_seconds = librosa.get_duration(
+                path=file_path
+            )
+
+            word_count = get_word_count(
+                transcript
+            )
+
+            wpm = calculate_wpm(
+                word_count,
+                duration_seconds
+            )
+
+            speed = get_speed_category(
+                wpm
+            )
+
+            fillers = detect_fillers(
+                transcript
+            )
+
+        st.success("Analysis Complete!")
+
+        col1, col2, col3 = st.columns(3)
+
+        col1.metric(
+            "Word Count",
+            word_count
+        )
+
+        col2.metric(
+            "WPM",
+            wpm
+        )
+
+        col3.metric(
+            "Speed",
+            speed
+        )
+
+        st.subheader("Filler Words")
+
+        st.json(fillers)
 
         st.subheader("Transcript")
 
